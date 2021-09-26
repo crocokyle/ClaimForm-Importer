3cf7bda91e08c06acf738245300bb718fe3d3fb5
crocokyle <me@kyle-dixon.com>
2021-09-26 08:27:20 -1000

    Using Azure.AI.FormRecognizer nuget package in FormRecognizer

--- ClaimForm-Importer/FormRecognizer.cs
+++ ClaimForm-Importer/FormRecognizer.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Net.Http;
 using System.Web;
 using System.IO;
+using Azure;
+using Azure.AI.FormRecognizer;
 
 namespace CSHttpClientSample
 {
@@ -11,35 +13,15 @@ namespace CSHttpClientSample
     {
         public static async void MakeRequest(string pdf)
         {
-            var client = new HttpClient();
-            var queryString = HttpUtility.ParseQueryString(string.Empty);
-            bool includeDetails = false;
+            // Grab environment variables
+            string endpoint = Environment.GetEnvironmentVariable("ACS_ENDPOINT");
+            string acsKey = Environment.GetEnvironmentVariable("ACS_KEY");
+            string modelId = Environment.GetEnvironmentVariable("MODEL_ID");
+            string firebaseKey = Environment.GetEnvironmentVariable("FIREBASE_KEY");
 
-            var acsKey = Environment.GetEnvironmentVariable("ACS_KEY");
-            var modelID = Environment.GetEnvironmentVariable("MODEL_ID");
-            var firebaseKey = Environment.GetEnvironmentVariable("FIREBASE_KEY");
-
-            // Request headers
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", acsKey);
-
-            // Request parameters
-            var uri = "https://eastus.api.cognitive.microsoft.com/formrecognizer/v2.1-preview.3/custom/models/" +
-                modelID + "/analyze?includeTextDetails=" + includeDetails;
-            Console.WriteLine(uri);
-
-            HttpResponseMessage response;
-
-            // Request body
-            var rawPDF = File.ReadAllText(pdf);
-
-            byte[] byteData = Encoding.UTF8.GetBytes(rawPDF);
-
-            using (var content = new ByteArrayContent(byteData))
-            {
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
-                response = await client.PostAsync(uri, content);
-                Console.WriteLine(response);
-            }
+            // Create a FormRecognizerClient
+            var credential = new AzureKeyCredential(acsKey);
+            var client = new FormRecognizerClient(new Uri(endpoint), credential);
 
         }
     }
