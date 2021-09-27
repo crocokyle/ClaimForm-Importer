a0a6eefef80695b53fae83412d4072bae1b4a1fe
crocokyle <me@kyle-dixon.com>
2021-09-26 14:25:52 -1000

    Code Cleanup

---
+++ ClaimForm-Importer/Firebase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ClaimForm_Importer
+{
+    static class Firebase
+    {
+        public static async Task<HttpResponseMessage> PostData(Dictionary<string, string> data)
+        {
+            string firebaseUrl = Environment.GetEnvironmentVariable("FIREBASE_URL") + "CMS1500.json";
+            Console.WriteLine($"Sending data to {firebaseUrl}...");
+
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(
+                    firebaseUrl,
+                     new StringContent(json, Encoding.UTF8, "application/json"));
+
+                return response;
+            }
+        }
+    }
+}
---
+++ ClaimForm-Importer/FormHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.FormRecognizer;
+using Azure.AI.FormRecognizer.Models;
+
+namespace ClaimForm_Importer
+{
+    static class FormHandler
+    {
+        public static async Task<Dictionary<string, string>> SendForm(string pdfPath, double formConfidenceThreshold, double fieldConfidenceThreshold)
+        {
+            // Create pdf FileStream
+            using var stream = new FileStream(pdfPath, FileMode.Open);
+
+            // Get environment variables
+            string endpoint = Environment.GetEnvironmentVariable("ACS_ENDPOINT");
+            string acsKey = Environment.GetEnvironmentVariable("ACS_KEY");
+            string modelId = Environment.GetEnvironmentVariable("MODEL_ID");
+
+            // Create a FormRecognizerClient
+            var credential = new AzureKeyCredential(acsKey);
+            var client = new FormRecognizerClient(new Uri(endpoint), credential);
+
+            // Send the filestream to ACS and wait for response
+            RecognizeCustomFormsOperation operation = await client.StartRecognizeCustomFormsAsync(modelId, stream);
+            Response<RecognizedFormCollection> operationResponse = await operation.WaitForCompletionAsync();
+            RecognizedFormCollection forms = operationResponse.Value;
+            Dictionary<string, string> formData = new Dictionary<string, string> { };
+
+            // Iterate through the forms in the response
+            foreach (RecognizedForm form in forms)
+            {
+                Console.WriteLine($"Form of type: {form.FormType}");
+
+                // Check for low form confidence
+                if (form.FormTypeConfidence.Value < formConfidenceThreshold)
+                {
+                    Console.WriteLine($"{pdfPath} confidence is below the threshold ({formConfidenceThreshold}).");
+                    continue;
+                }
+
+                if (form.FormTypeConfidence.HasValue)
+                    Console.WriteLine($"Form type confidence: {form.FormTypeConfidence.Value}");
+
+                // Iterate through the fields in the response
+                Console.WriteLine($"Form was analyzed with model with ID: {form.ModelId}");
+                foreach (FormField field in form.Fields.Values)
+                {
+                    string value = "";
+
+                    // Is the field a SelectionMark?
+                    // NOTE: field confidence is very high on SelectionMarks, but a check could be added
+                    if (field.Value.ValueType.ToString() == "SelectionMark")
+                    {
+                        value = field.Value.AsSelectionMarkState().ToString();
+                    }
+                    else
+                    {
+                        // Check for empty field values
+                        if (field.ValueData == null)
+                        {
+                            Console.WriteLine($"{field.Name} is empty. Please manually enter the value:");
+                            value = Console.ReadLine();
+                        }
+
+                        // Check for low field confidence
+                        else if (field.Confidence < fieldConfidenceThreshold)
+                        {
+                            Console.WriteLine($"{field.Name} confidence({field.Confidence}) is below the threshold ({fieldConfidenceThreshold}).");
+                            Console.WriteLine($"Got \"{field.ValueData.Text}\", please manually enter the correct value.");
+
+                            value = Console.ReadLine();
+                        }
+
+                        // Assign the value for the field to what was guessed by ACS
+                        else
+                        {
+                            value = field.ValueData.Text;
+                        }
+                    }
+                    // Add the determined value to the dictionary
+                    formData.Add(field.Name, value);
+                }
+            }
+            return formData;
+        }
+    }
+}
